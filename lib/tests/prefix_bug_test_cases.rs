c742c8dcc43fd8ffe4e2bcb4e2f5123b02f9b3dd
Dustin Whitney <dustin.whitney@gmail.com>
2019-10-12 15:48:59 -0400

    added a test case to illustrate a bug

---
+++ lib/tests/prefix_bug_test_cases.rs
@@ -0,0 +1,68 @@
+use rudf::{DatasetSyntax, Repository, RepositoryConnection, MemoryRepository};
+use rudf::sparql::{PreparedQuery, QueryResult};
+
+#[test]
+fn prefix_bug_test() {
+  let repository = MemoryRepository::default();
+  let mut connection = repository.connection().unwrap();
+  let ttl = br#"
+   <http://example.com/bob> <http://xmlns.com/foaf/0.1/name> "Bob" <http://service1.org> .
+   <http://example.com/alice> <http://xmlns.com/foaf/0.1/name> "Alice" <http://service1.org>.
+
+   <http://example.com/bob> <http://xmlns.com/foaf/0.1/mbox> <mailto:bob@example.org> <http://service2.org> .
+   <http://example.com/alice> <http://xmlns.com/foaf/0.1/mbox> <mailto:alice@example.org> <http://service2.org> .
+  "#;
+  connection.load_dataset(ttl.as_ref(), DatasetSyntax::NQuads, None).unwrap();
+
+  let bug_query = r#"
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    SELECT ?name ?mbox
+    WHERE
+      { 
+        GRAPH <http://service1.org>
+        { ?s <foaf:name> ?name 
+        }
+        GRAPH <http://service2.org>
+        { ?s <foaf:mbox> ?mbox 
+        }
+      }
+    "#;
+
+
+  let query = r#"
+    SELECT ?name ?mbox
+    WHERE
+      { 
+        GRAPH <http://service1.org>
+        { ?s <http://xmlns.com/foaf/0.1/name> ?name 
+        }
+        GRAPH <http://service2.org>
+        { ?s <http://xmlns.com/foaf/0.1/mbox> ?mbox 
+        }
+      }
+    "#;
+
+
+  let prepared_query = connection.prepare_query(query, None).unwrap();
+  let results_query = prepared_query.exec().unwrap();
+
+  let query_set = if let QueryResult::Bindings(results) = results_query {
+    results.into_values_iter().map(move |b| b.unwrap()).collect::<Vec<_>>()
+  } else {
+    vec![]
+  };
+
+
+  let prepared_bug = connection.prepare_query(bug_query, None).unwrap();
+  let results_bug = prepared_bug.exec().unwrap();
+
+  let bug_set = if let QueryResult::Bindings(results) = results_bug {
+    results.into_values_iter().map(move |b| b.unwrap()).collect::<Vec<_>>()
+  } else {
+    vec![]
+  };
+
+  assert_eq!(query_set, bug_set);
+  
+}
+
